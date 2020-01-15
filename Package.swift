1f66e366333158832fafe7488913fe2cb43a9288
Pavel H <pavel.h@smartum.pro>
2020-01-15 17:35:11 +0300

    update package file

--- Package.swift
+++ Package.swift
@@ -8,8 +8,8 @@ let package = Package(
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "eosio",
-            targets: ["eosio"]),
+            name: "EosioSwift",
+            targets: ["EosioSwift"]),
     ],
     dependencies: [
         .package(url: "https://github.com/attaswift/BigInt.git", from: "5.0.0"),
@@ -19,10 +19,10 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "eosio",
+            name: "EosioSwift",
             dependencies: ["BigInt", "PromiseKit"]),
         .testTarget(
-            name: "eosioTests",
-            dependencies: ["eosio"]),
+            name: "EosioSwiftTests",
+            dependencies: ["EosioSwift"]),
     ]
 )
